d359dc73f6b8440c918f6668c0d78cd7b6128bca
Sadja18 <namanmishraec1045@gmail.com>
2022-04-05 14:43:13 +0530

    academic version: 0.0.0.0

---
+++ academic/uploadinspection.php
@@ -0,0 +1,46 @@
+<?php
+require_once '../ripcord/ripcord.php';
+require_once '../envRead.php';
+
+use sadja\DotEnv;
+
+header('Access-Control-Allow-Origin: ' . $_SERVER['REMOTE_ADDR'], false);
+
+header('Content-Type: application/json');
+
+header('Access-Control-Allow-Methods: GET, POST');
+
+header("Access-Control-Allow-Headers: X-Requested-With");
+
+(new DotEnv(__DIR__ . '/.env'))->load();
+
+// $url = getenv('PUBLICALT');
+// $url = getenv('PRIVATEALT');
+// $url = getenv('PUBLIC');
+$url = getenv('PRIVATE');
+
+$failNotPost = array(
+    'message' => 'Invalid Request',
+);
+
+$failNoData = array(
+    'message' => 'Please pass required parameters',
+);
+
+$failInvalidCredentials = array(
+    "message" => "Invalid Credentials",
+);
+
+$failedLogin = array(
+    "message" => "Login Failure. This user does not have the required access rights.",
+);
+if ($_SERVER['REQUEST_METHOD'] == 'POST' || $_SERVER['REQUEST_METHOD'] == 'post') {
+    echo json_encode(
+        array(
+            'message' => 'PACE exam inspection data upload request received',
+            'data' => array(),
+        )
+    );
+} else {
+    echo json_encode($failNotPost);
+}
