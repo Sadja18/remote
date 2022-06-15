0472fd6c31dd8139d57cddb0be50a5730cb15c41
Sadja18 <namanmishraec1045@gmail.com>
2022-06-15 09:11:28 +0300

    updated school user login

--- online/login.php
+++ online/login.php
@@ -5,14 +5,12 @@ header('Access-Control-Allow-Origin: *', false);
 header('Access-Control-Allow-Methods: GET, POST');
 
 header("Access-Control-Allow-Headers: X-Requested-With");
+header('Content-Type: application/json');
 
-ini_set('display_startup_errors', 1);
-ini_set('display_errors', 1);
-error_reporting(-1);
-
+// require_once './extra.php';
 $login_status = null;
 $userID = null;
-$dbname = null;
+$dbname = 'doednhdd';
 $userName = null;
 $userPassword = null;
 
@@ -25,53 +23,85 @@ $publicURL = "http://14.139.180.56:8069";
 
 $url = $publicURL;
 
-
-// session_start();
-require_once './extra.php';
-require_once './ripcord/ripcord.php';
-header('Access-Control-Allow-Origin: *');
-
-header('Content-Type: application/json');
-
-header('Access-Control-Allow-Methods: GET, POST');
-
-header("Access-Control-Allow-Headers: X-Requested-With");
-
 $arr = array();
+require_once './ripcord/ripcord.php';
 
-if(isSiteAvailible($url)){
-    if ($_SERVER['REQUEST_METHOD'] == "POST") {
+if ($_SERVER['REQUEST_METHOD'] == "POST" || $_SERVER['REQUEST_METHOD'] == 'GET') {
+    // file_get_contents will use read only raw data stream php://input
+    // to get a json like data object
+    $entityBodyJSON = file_get_contents('php://input');
+    // decode the afore mentoned json like object
+    $entityBody = json_decode($entityBodyJSON);
+    // echo json_encode(
+    //     array(
+    //         "p"=> $entityBody,
+    //         'g'=> gettype($entityBody),
+    //     )
+    // );
 
-        // file_get_contents will use read only raw data stream php://input
-        // to get a json like data object
-        $entityBodyJSON = file_get_contents('php://input');
+    $userName = $entityBody-> user;
+    $userPassword = $entityBody->password;
     
-        // decode the afore mentoned json like object
-        $entityBody = json_decode($entityBodyJSON, true);
-    
-        $userName = $entityBody['user'];
-        $userPassword = $entityBody['password'];
-    
-        if (isset($entityBody['dbname'])) {
-            $dbname = $entityBody['dbname'];
-        } else {
-            $dbname = 'doednhdd';
-        }
-    
-        $common = ripcord::client($url . '/xmlrpc/2/common');
-    
-    
-        $userID = $common->authenticate($dbname, $userName, $userPassword, array());
-    
-        if (empty($userID) or !isset($userID) or $userID == 0 or $userID == false) {
+    if (isset($entityBody->dbname)) {
+        $dbname = $entityBody->dbname;
+    } else {
+        $dbname = 'doednhdd';
+    }
+
+    $common = ripcord::client($url . '/xmlrpc/2/common');
+
+    // echo "13";
+
+    $userID = $common->authenticate($dbname, $userName, $userPassword, array());
+
+
+    if (empty($userID) || !isset($userID) || $userID == 0 || $userID == false) {
+
+        $arr = array(
+            "message" => "Invalid credentials",
+            'login_status' => 0,
+            'dbname' => $dbname
+        );
+        echo json_encode($arr);
+    } else {
+        
+        $models = ripcord::client("$url/xmlrpc/2/object");
+        // sleep(1);
+        
+        $record = $models->execute_kw(
+            $dbname,
+            $userID,
+            $userPassword,
+            'school.school',
+            'search_read',
+            array(
+                array(
+                    array(
+                        'email', '=', $userName,
+                    )
+                ),
+            ),
+            array(
+                'fields' => array('email', 'com_name'),
+            ),
+        );
+        
+        if (isset($record) && !isset($record['faultString']) && isset($record[0]['id'])) {
+            /// is headmaster
             $arr = array(
-                'login_status' => 0,
-                'dbname'=> $dbname
+                'user' => $entityBody['user'],
+                'password' => $entityBody['password'],
+                'dbname' => $dbname,
+                'login_status' => 1,
+                'userID' => $userID,
+                'headMaster' => 'yes',
+                'schoolId' => $record[0]['id'],
+                'isOnline' => 1
             );
+            echo json_encode($arr);
         } else {
-            $models = ripcord::client("$url/xmlrpc/2/object");
-            sleep(1);
-            $record = $models->execute_kw(
+            /// is not headmaster
+            $school = $models->execute_kw(
                 $dbname,
                 $userID,
                 $userPassword,
@@ -80,66 +110,35 @@ if(isSiteAvailible($url)){
                 array(
                     array(
                         array(
-                            'email', '=', $userName,
+                            'name', '!=', FALSE
                         )
-                    ),
+                    )
                 ),
                 array(
-                    'fields'=>array('email', 'com_name'),
+                    'fields' => array(
+                        'com_name'
+                    ),
                 ),
             );
-            if(isset($record) && !isset($record['faultString']) && isset($record[0]['id'])){
-                $arr = array(
-                    'user' => $entityBody['user'],
-                    'password' => $entityBody['password'],
+            
+            $schoolId = $school[0]['id'];
+            
+            echo json_encode(
+                array(
+                    'user' => $entityBody->user,
+                    'password' => $entityBody->password,
                     'dbname' => $dbname,
                     'login_status' => 1,
                     'userID' => $userID,
-                    'headMaster'=> 'yes',
-                    'schoolId'=> $record[0]['id'],
+                    'schoolId' => $school[0]['id'],
+                    'headMaster' => 'no',
                     'isOnline' => 1
-                );
-    
-            }else{
-                sleep(1);
-                $school = $models->execute_kw(
-                    $dbname,
-                    $userID,
-                    $userPassword,
-                    'school.school',
-                    'search_read',
-                    array(
-                        array(
-                            array(
-                                'name', '!=', FALSE
-                            )
-                        )
-                    ),
-                    array(
-                        'fields'=> array(
-                            'com_name'
-                        ),
-                    ),
-                );
-                $schoolId = $school[0]['id'];
-                $arr = array(
-                    'user' => $entityBody['user'],
-                    'password' => $entityBody['password'],
-                    'dbname' => $dbname,
-                    'login_status' => 1,
-                    'userID' => $userID,
-                    'schoolId'=> $school[0]['id'],
-                    'headMaster'=> 'no',
-                    'isOnline' => 1
-                );
-            }
+                )
+            );
         }
-    
-        // header('Access-Control-Allow-Origin: http://10.184.6.81', false);
-    
-       
-        echo json_encode($arr);
     }
-}else{
-    echo json_encode(serverUnReachable());
+} else {
+    echo json_encode(array(
+        "message" => "failed", "code" => "not post request", 'j' => $_SERVER['REQUEST_METHOD']
+    ));
 }
